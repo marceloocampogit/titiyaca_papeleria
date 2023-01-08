397a6d7357f3d100a81ab900b2ea30e46ddf2560
marceloocampogit <marceloocampo1993@icloud.com>
2023-01-08 20:48:42 -0300

    Me habia faltado el product code

--- papeleria_titiyaca/products/models.py
+++ papeleria_titiyaca/products/models.py
@@ -6,6 +6,7 @@ class Categories(models.Model):
 
 class Products(models.Model):
     product_name = models.CharField(max_length=100)
+    product_code = models.IntegerField(max_length=10)
     category_name = models.ForeignKey(Categories, on_delete= models.CASCADE)
     product_price = models.FloatField()
     product_description = models.CharField(max_length=200)
