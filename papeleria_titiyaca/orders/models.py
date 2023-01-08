7c12265a4ccdfd9775373b41adbb46cb93f018d8
crodriguez <yacamundi@gmail.com>
2023-01-08 20:37:14 -0300

    primer paso ordenes

--- papeleria_titiyaca/orders/models.py
+++ papeleria_titiyaca/orders/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from products.models import Products
 
 # Create your models here.
+
+class OrderStatus(models.Model):
+    status = models.CharField(max_length=20)
+    
+class PaymentMethod(models.Model):
+    payment_method_large = models.CharField(max_length=20)
+    payment_method_short = models.CharField(max_length=3)
+
+class Orders(models.Model):
+    order_code = models.IntegerField(max_length=10)    
+    order_price = models.FloatField()
+    creation_time = models.DateTimeField(auto_now_add=True)    
+    payment_method_short = models.ForeignKey(PaymentMethod, on_delete= models.CASCADE)     
+    status = models.ForeignKey(OrderStatus, on_delete= models.CASCADE)
+
+class OrderItems(models.Model):
+    order_code = models.ForeignKey(Orders, on_delete= models.CASCADE)     
+    item_code = models.IntegerField(max_length=3)
+    product_code = models.ForeignKey(Products, on_delete= models.CASCADE)
