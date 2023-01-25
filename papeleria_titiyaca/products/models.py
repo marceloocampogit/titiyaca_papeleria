e1bae828fcae83697e1f6a035991bb76cda05414
crodriguez <yacamundi@gmail.com>
2023-01-24 22:42:30 -0300

    sin necesidad 2

--- papeleria_titiyaca/products/models.py
+++ papeleria_titiyaca/products/models.py
@@ -6,7 +6,7 @@ class Categories(models.Model):
     category_name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.category_code
+        return str(self.category_code)
 
 class Products(models.Model):
     product_name = models.CharField(max_length=100)
@@ -16,4 +16,4 @@ class Products(models.Model):
     product_description = models.CharField(max_length=200)
     
     def __str__(self):
-        return self.product_code
+        return str(self.product_code)
