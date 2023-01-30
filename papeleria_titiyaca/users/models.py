f45edee5508e579575e993aa2ff4174a33cd83fc
marceloocampogit <marceloocampo1993@icloud.com>
2023-01-30 19:55:16 -0300

    Agregue algo de users

--- papeleria_titiyaca/users/models.py
+++ papeleria_titiyaca/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class ProfileUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    country = models.CharField(max_length=50, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
+
+    def __str__(self):
+        return self.user.first_name + ' ' + self.user.last_name
